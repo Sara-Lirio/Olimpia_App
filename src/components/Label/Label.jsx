e6c493636cce0d485bec14913af817b5da5f6e73
est8r <estermessias6@gmail.com>
2022-09-04 00:36:59 -0300

    estilizando componente label com styled-components

--- src/components/Label/Label.jsx
+++ src/components/Label/Label.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Label = ({ className, texto }) => {
   return <label className={className}>{texto}</label>;
 };
 
-export default Label;
+const StyleLabel = styled(Label)`
+  color: gray;
+  margin-bottom: 10px;
+`;
+
+export default StyleLabel;
