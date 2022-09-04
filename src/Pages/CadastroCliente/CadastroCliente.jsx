83ed96975b8e99683a55bbca8aa123c4867d4bfc
est8r <estermessias6@gmail.com>
2022-09-04 00:40:50 -0300

    criando tags de base e estilizações com styled-components

--- src/Pages/CadastroCliente/CadastroCliente.jsx
+++ src/Pages/CadastroCliente/CadastroCliente.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/Header/Header.jsx';
+
+const DivTitulos = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 0.5rem;
+  background-color: #e9e9e9;
+  align-items: flex-start;
+  border-radius: 15px;
+  padding-left: 35px;
+`;
+
+const DivGeral = styled.form`
+  margin: 0 auto;
+  display: grid;
+  width: 70%;
+  height: 600px;
+  grid-template-columns: 35% 65%;
+  grid-template-rows: repeat(1, 1fr);
+  border-radius: 15px;
+  margin-top: 40px;
+  box-shadow: 0px 0px 18px 5px rgba(0, 0, 0, 0.08);
+`;
 
 const CadastroCliente = () => {
   return (
-    <div><Header/></div>
-  )
-}
+    <div>
+      <Header />
+      <DivGeral>
+        <DivTitulos></DivTitulos>
+      </DivGeral>
+    </div>
+  );
+};
 
-export default CadastroCliente
+export default CadastroCliente;
