e752cc0f5f8289e209f8d7e688aad3e5cf811f5f
est8r <estermessias6@gmail.com>
2022-09-04 00:35:15 -0300

    estilizando componente input com styled components

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Input = ({ type, onChange, placeholder, className }) => {
   return (
@@ -11,4 +12,13 @@ const Input = ({ type, onChange, placeholder, className }) => {
   );
 };
 
-export default Input;
+const StyleInput = styled(Input)`
+  width: 100%;
+  height: 40px;
+  border-radius: 5px;
+  border: none;
+  background-color: #eeeeee;
+  padding-left: 8px;
+`;
+
+export default StyleInput;
