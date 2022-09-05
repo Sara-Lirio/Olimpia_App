b36a7fdc245cc0603581a2c647954f9f694fe8ea
est8r <estermessias6@gmail.com>
2022-09-05 01:16:36 -0300

    importando e utilizando os componentes Label e Input

--- src/components/FormClientes/FormClientes.jsx
+++ src/components/FormClientes/FormClientes.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Form, Row, Column, DivInput, Button } from './FormStyle.js';
+import Label from '../../components/Label/Label.jsx';
+import Input from '../../components/Input/Input.jsx';
 
 const FormClientes = () => {
   return (
@@ -7,25 +8,46 @@ const FormClientes = () => {
       <Form>
         <Row>
           <Column mobile="6" tablet="12" desktop="6">
-            <DivInput></DivInput>
+            <DivInput>
+              <Label texto="Nome" />
+              <Input type="text" placeholder="Digite seu nome.." />
+            </DivInput>
           </Column>
           <Column mobile="6" tablet="12" desktop="6">
-            <DivInput></DivInput>
+            <DivInput>
+              <Label texto="CPF" />
+              <Input type="text" placeholder="Digite seu CPF.." />
+            </DivInput>
           </Column>
           <Column mobile="12" tablet="12" desktop="12">
-            <DivInput></DivInput>
+            <DivInput>
+              <Label texto="Email" />
+              <Input type="text" placeholder="Digite seu email.." />
+            </DivInput>
           </Column>
           <Column mobile="6" tablet="12" desktop="6">
-            <DivInput className=""></DivInput>
+            <DivInput className="">
+              <Label texto="Senha" />
+              <Input type="password" placeholder="Digite sua senha.." />
+            </DivInput>
           </Column>
           <Column mobile="6" tablet="12" desktop="6">
-            <DivInput className=""></DivInput>
+            <DivInput className="">
+              <Label texto="Confirme sua senha" />
+              <Input type="password" placeholder="Digite sua senha.." />
+            </DivInput>
           </Column>
           <Column mobile="6" tablet="12" desktop="6">
-            <DivInput className=""></DivInput>
+            <DivInput className="">
+              <Label texto="Telefone" />
+              <Input type="text" placeholder="Digite seu telefone.." />
+            </DivInput>
           </Column>
           <Column mobile="6" tablet="12" desktop="6">
-            <DivInput className=""></DivInput>
+            <DivInput className="">
+              <Label texto="CEP" />
+              <Input type="text" placeholder="Digite seu CEP.." />
+            </DivInput>
           </Column>
         </Row>
         <Button type="submit">Enviar</Button>
