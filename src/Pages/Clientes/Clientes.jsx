0068e2b51df18760deff6a7ea4157f885d0c52b5
est8r <estermessias6@gmail.com>
2022-09-06 18:43:02 -0300

    criação do cabeçalho da tabela de clientes

--- src/Pages/Clientes/Clientes.jsx
+++ src/Pages/Clientes/Clientes.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
+import React from 'react';
+import Header from '../../components/Header/Header';
+import { Div } from '../../styles/global.js';
+import { Cell, Row } from '../../styles/global.js';
+import TabelaClientes from '../../components/TabelaClientes/TabelaClientes.jsx';
 
 const Clientes = () => {
   return (
-    <div>
-      <Header/>
-      clientes
+    <div style={{ textAlign: 'center' }}>
+      <Header />
+      <div></div>
+      <Div>
+        <th>
+          <Row>
+            <Cell align="left">id</Cell>
+            <Cell align="left">Nome do cliente</Cell>
+            <Cell align="left">E-mail</Cell>
+            <Cell align="left">CPF</Cell>
+            <Cell align="left">Senha</Cell>
+            <Cell align="left">Telefone</Cell>
+            <Cell align="left">CEP</Cell>
+            <Cell align="left">Atualizar</Cell>
+            <Cell align="left">Deletar</Cell>
+          </Row>
+        </th>
+      </Div>
     </div>
-  )
-}
+  );
+};
 
-export default Clientes
+export default Clientes;
