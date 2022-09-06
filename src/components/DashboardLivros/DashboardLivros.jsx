e5f1589f52aa462a1887dbc30924fab9ee7dbcd4
nathuchoa <97313973+nathuchoa@users.noreply.github.com>
2022-09-06 02:23:30 +0100

    Update dashboard Livros (sem comentários)

--- src/components/DashboardLivros/DashboardLivros.jsx
+++ src/components/DashboardLivros/DashboardLivros.jsx
@@ -5,14 +5,8 @@ import ItemLivros from "../ItemLivros/ItemLivros";
 import S from "./DashboardLivros.module.css";
 
 const DashboardLivros = () => {
-  const [name, setName] = useState("");
   const [livros, setLivros] = useState([]);
   const [atualizar, setAtualizar] = useState(false);
-  // const [modalAction, setModalAction] = useState({
-  //   postLivro: false,
-  //   editarLivro: false,
-  //   deletarLivro: false,
-  // });
 
   async function requisicao() {
     const response = await getLivros();
@@ -22,51 +16,6 @@ const DashboardLivros = () => {
   function handleAtualizar() {
     setAtualizar(true);
   }
-
-  // const handleAbrirModal = (key) => {
-  //   modalAction[key]
-  //     ? setModal({ ...modalAction, [key]: false })
-  //     : setModal({ ...modalAction, [key]: true });
-  // };
-
-  // async function postLivro() {
-  //   await cadastrarLivro();
-  //   setAtualizar(true);
-  //   handleAbrirModal("postLivro");
-  // }
-
-  // const hookAtualizaLivro = (idLivro) => {
-  //   setLivros({ idLivro: idLivro });
-  //   handleAbrirModal("editarLivro");
-  // };
-
-  // async function atualizarLivro() {
-  //   await putLivro(livro.idLivro, novoLivro);
-  //   handleAbrirModal("editarLivro");
-  //   setAtualizar(true);
-  // }
-
-  // const hookDeletaLivro = (itemLivro) => {
-  //   setLivros({
-  //     idLivro: itemLivro.idLivro,
-  //     imagem: itemLivro.imagem,
-  //     titulo: itemLivro.titulo,
-  //     autor: itemLivro.autor,
-  //     genero: itemLivro.genero,
-  //     formato: itemLivro.formato,
-  //     valor: itemLivro.valor,
-  //     idioma: itemLivro.idioma,
-  //     qtdEstoque: itemLivro.qtdEstoque,
-  //     nPaginas: itemLivro.nPaginas,
-  //   });
-  //   handleAbrirModal("deletarLivro");
-  // };
-
-  // async function deletaLivro(idLivro) {
-  //   await deletaLivro(idLivro);
-  //   handleAbrirModal("deletarLivro");
-  //   setAtualizar(true);
-  // }
 
   useEffect(() => {
     requisicao();
@@ -101,22 +50,7 @@ const DashboardLivros = () => {
             );
           })}
       </section>
-      {/* <Modal
-          open={modal.deletaLivro}
-          onClose={()=> handleAbrirModal('deletaLivro')}
-        >
-          <Box>
-            <p>Quer mesmo excluir o  livro {itemLivro.titulo}?</p>
-            <p>do banco de dados?</p>
-            <Button onClick={()=> deletaLivro(itemLivro.idLivro)} color="error" variant="contained">Deletar</Button>
-            <Button onClick={()=> handleAbrirModal('deletaLivro')} color="primary" variant="contained">Voltar</Button>
-          </Box>
-        </Modal>
-        <Modal
-          open={modal.editarLivro}
-          onClose={()=> handleAbrirModal('editarLivro')}
-        >
-        </Modal> */}
+
     </div>
   );
 };
