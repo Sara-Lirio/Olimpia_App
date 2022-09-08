aae120d456a3648704059c7ed5dc7121baa3f9c6
nathuchoa <97313973+nathuchoa@users.noreply.github.com>
2022-09-08 21:29:28 +0100

    Uptade layout dos formulario para livros

--- src/components/FormLivros/FormLivros.jsx
+++ src/components/FormLivros/FormLivros.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Form, Row, Column, DivInput, Button } from './FormStyle.js'
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import S from "./FormLivros.module.css";
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import Label from "../Label/Label";
 
 const FormLivros = () => {
@@ -31,117 +39,121 @@ const FormLivros = () => {
   };
 
   return (
-    <div>
-      <Form>
-        <Row>
-            <DivInput>
-              <Label texto="Titulo" />
-              <Input
-                type="text"
-                value={dadosForm.titulo}
-                placeholder="Insira o título..."
-                onChange={({ target }) => handleChange(target.value, "titulo")}
-              />
-            </DivInput>
-
-            <DivInput>
-              <Label texto="Imagem" />
-              <Input
-                type="text"
-                value={dadosForm.imagem}
-                placeholder="Cole a URL..."
-                onChange={({ target }) => handleChange(target.value, "imagem")}
-              />
-            </DivInput>
-
-            <DivInput>
-              <Label texto="Autor" />
-              <Input
-                type="text"
-                value={dadosForm.autor}
-                placeholder="Digite o nome..."
-                onChange={({ target }) => handleChange(target.value, "autor")}
-              />
-            </DivInput>
-
-            <DivInput>
-              <Label texto="Genero" />
-              <Input
-                type="text"
-                value={dadosForm.genero}
-                placeholder="Digite o gênero..."
-                onChange={({ target }) => handleChange(target.value, "genero")}
-              />
-            </DivInput>
-
-            <DivInput>
-              <Label texto="Formato" />
-              <FormControl>
-                <FormLabel id="demo-controlled-radio-buttons-group">
-                  Formato
-                </FormLabel>
-                <RadioGroup
-                  aria-labelledby="demo-controlled-radio-buttons-group"
-                  name="controlled-radio-buttons-group"
-                  value={dadosForm.genero}
-                  onChange={({ target }) =>
-                    handleChange(target.value, "formato")
-                  }
-                >
-                  <FormControlLabel
-                    value="físico"
-                    control={<Radio />}
-                    label="Físico"
-                  />
-                  <FormControlLabel
-                    value="online"
-                    control={<Radio />}
-                    label="Online"
-                  />
-                </RadioGroup>
-              </FormControl>
-            </DivInput>
-
-            <DivInput>
-              <Label texto="Idioma" />
-              <Input
-                type="text"
-                value={dadosForm.idioma}
-                placeholder="Digite o idioma..."
-                onChange={({ target }) => handleChange(target.value, "idioma")}
-              />
-            </DivInput>
-
-            <DivInput>
-              <Label texto="Qdt no Estoque" />
-              <Input
-                type="text"
-                value={dadosForm.qtdEstoque}
-                placeholder="Apenas números"
-                onChange={({ target }) =>
-                  handleChange(target.value, "qtdEstoque")
-                }
-              />
-            </DivInput>
-
-            <DivInput>
-              <Label texto="Número de Páginas" />
-              <Input
-                type="text"
-                value={dadosForm.nPaginas}
-                placeholder="Apenas números"
-                onChange={({ target }) =>
-                  handleChange(target.value, "nPaginas")
-                }
-              />
-            </DivInput>
-        </Row>
-        <div>
-          <Button onClick={handleCreate} type="submit">
-            Enviar
-          </Button>
+    <div className={S.boxContainer}>
+      <form className={S.formContainer}>
+        <div id={S.capalivro}>
+          <img src={dadosForm.imagem} height={150} />
         </div>
-      </Form>
+
+        <div id={S.imagem}>
+          <Label texto="Imagem" />
+          <Input
+            type="text"
+            value={dadosForm.imagem}
+            placeholder="Cole a URL..."
+            onChange={({ target }) => handleChange(target.value, "imagem")}
+          />
+        </div>
+
+        <div id={S.titulo}>
+          <Label texto="Titulo" />
+          <Input
+            type="text"
+            value={dadosForm.titulo}
+            placeholder="Insira o título..."
+            onChange={({ target }) => handleChange(target.value, "titulo")}
+          />
+        </div>
+
+        <div id={S.autor}>
+          <Label texto="Autor" />
+          <Input
+            type="text"
+            value={dadosForm.autor}
+            placeholder="Digite o nome..."
+            onChange={({ target }) => handleChange(target.value, "autor")}
+          />
+        </div>
+
+        <div id={S.valor}>
+          <Label texto="Preço: R$" />
+          <Input
+            type="number"
+            value={dadosForm.valor}
+            onChange={({ target }) => handleChange(target.value, "valor")}
+          />
+        </div>
+
+        <div id={S.genero}>
+          <Label texto="Genero" />
+          <Input
+            type="text"
+            value={dadosForm.genero}
+            placeholder="Digite o gênero..."
+            onChange={({ target }) => handleChange(target.value, "genero")}
+          />
+        </div>
+
+        <div id={S.formato}>
+          <FormControl>
+            <FormLabel id="demo-controlled-radio-buttons-group">
+              Formato
+            </FormLabel>
+            <RadioGroup id={S.radio}
+              aria-labelledby="demo-controlled-radio-buttons-group"
+              name="controlled-radio-buttons-group"
+              value={dadosForm.genero}
+              onChange={({ target }) => handleChange(target.value, "formato")}
+            >
+              <FormControlLabel
+                value="físico"
+                control={<Radio />}
+                label="Físico"
+              />
+              <FormControlLabel
+                value="online"
+                control={<Radio />}
+                label="Online"
+              />
+            </RadioGroup>
+          </FormControl>
+        </div>
+
+        <div id={S.idioma}>
+          <Label texto="Idioma" />
+          <Input
+            type="text"
+            value={dadosForm.idioma}
+            placeholder="Digite o idioma..."
+            onChange={({ target }) => handleChange(target.value, "idioma")}
+          />
+        </div>
+
+        <div id={S.estoque}>
+          <Label texto="Qdt no Estoque" />
+          <Input
+            type="number"
+            value={dadosForm.qtdEstoque}
+            placeholder="Apenas números"
+            onChange={({ target }) => handleChange(target.value, "qtdEstoque")}
+          />
+        </div>
+
+        <div id={S.paginas}>
+          <Label texto="nº Páginas" />
+          <Input
+            type="number"
+            value={dadosForm.nPaginas}
+            placeholder="Apenas números"
+            onChange={({ target }) => handleChange(target.value, "nPaginas")}
+          />
+        </div>
+      </form>
+      <div className={S.btncontainer}>
+        <Button variant="contained" onClick={handleCreate} type="submit">
+          Enviar
+        </Button>
+      </div>
     </div>
   );
 };
