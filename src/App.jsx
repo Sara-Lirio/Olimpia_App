458f2573cd92ce60e02f14c0769ba7f25f133ef0
est8r <estermessias6@gmail.com>
2022-09-09 00:14:47 -0300

    adicionando rota de atualiza clientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import './styles/reset.css'
-import './styles/global.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Clientes from './Pages/Clientes/Clientes'
-import Footer from './components/Footer/Footer'
-import Livros from './Pages/Livros/Livros'
-import Equipe from './Pages/Equipe/Equipe'
-import CadastroCliente from './Pages/CadastroCliente/CadastroCliente'
-import CadastroLivro from './Pages/CadastroLivro/CadastroLivro'
-import FormLivros from './components/FormLivros/FormLivros'
-import HomeLogin from './Pages/HomeLogin/HomeLogin'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Clientes from './Pages/Clientes/Clientes';
+import Footer from './components/Footer/Footer';
+import Livros from './Pages/Livros/Livros';
+import Equipe from './Pages/Equipe/Equipe';
+import CadastroCliente from './Pages/CadastroCliente/CadastroCliente';
+import CadastroLivro from './Pages/CadastroLivro/CadastroLivro';
+import HomeLogin from './Pages/HomeLogin/HomeLogin';
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomeLogin/>}/>
-        <Route path='/home' element={<Home/>} />
-        <Route path='/livros' element={<Livros />} />
-        <Route path='/clientes' element={<Clientes />} />
-        <Route path='/equipe' element={<Equipe />} />
-        <Route path='/cadastroCliente' element={<CadastroCliente />} />
-        <Route path='/cadastroLivro' element={<CadastroLivro />} />
+        <Route path="/" element={<HomeLogin />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/livros" element={<Livros />} />
+        <Route path="/clientes" element={<Clientes />} />
+        <Route path="/atualizaCliente/:id" element={<CadastroCliente />} />
+        <Route path="/equipe" element={<Equipe />} />
+        <Route path="/cadastroCliente" element={<CadastroCliente />} />
+        <Route path="/cadastroLivro" element={<CadastroLivro />} />
       </Routes>
 
-      <Footer /> 
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
