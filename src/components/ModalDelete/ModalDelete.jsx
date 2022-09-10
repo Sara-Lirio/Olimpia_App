ba541ea339b7c86497f326b61ee595bc1428a9aa
est8r <estermessias6@gmail.com>
2022-09-09 21:08:29 -0300

    estlizando modal de delete do cliente

--- src/components/ModalDelete/ModalDelete.jsx
+++ src/components/ModalDelete/ModalDelete.jsx
@@ -1,8 +1,12 @@
 import { Button, Modal, Box } from '@mui/material';
-
+import styled from 'styled-components';
 import React from 'react';
+import { BsFileX } from 'react-icons/bs';
 
 const style = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
   position: 'absolute',
   color: 'black',
   top: '50%',
@@ -13,7 +17,16 @@ const style = {
   border: '2px solid #000',
   boxShadow: '24',
   p: 4,
+  gap: 2,
+  textAlign: 'center',
 };
+
+const ButtonDiv = styled.div`
+  display: flex;
+  gap: 20px;
+  background-color: transparent;
+  border: none;
+`;
 
 const ModalDelete = ({ open, handleClose, onClick, fecharModal }) => {
   return (
@@ -26,12 +39,14 @@ const ModalDelete = ({ open, handleClose, onClick, fecharModal }) => {
       <Box sx={style}>
         <h2 id="parent-modal-title">Cuidado!</h2>
         <p>Tem certeza que deseja excluir esse cliente?</p>
-        <Button variant="contained" color="primary" onClick={fecharModal}>
-          Cancelar
-        </Button>
-        <Button variant="contained" color="warning" onClick={onClick}>
-          Excluir
-        </Button>
+        <ButtonDiv>
+          <Button variant="contained" color="primary" onClick={fecharModal}>
+            Cancelar
+          </Button>
+          <Button variant="contained" color="warning" onClick={onClick}>
+            Excluir
+          </Button>
+        </ButtonDiv>
       </Box>
     </Modal>
   );
